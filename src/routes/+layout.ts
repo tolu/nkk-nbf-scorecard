b50f3b221ca172e16f711cd8fb7ac65349a4f61d
Tobias Lundin <tobias.lundin@gmail.com>
2023-09-03 15:05:17 +0200

    add vercel web analytics

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,2 +1,10 @@
+import { dev } from '$app/environment';
+import { inject } from '@vercel/analytics';
+
+// instrument vercel analytics
+if (dev === false) {
+	inject({ mode: 'production' });
+}
+
 // Make it SPA
 export const ssr = false;
